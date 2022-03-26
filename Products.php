cea1e44530cbdd78603ccac2754a0e4ea18abf34
Incrediblebug20 <102393665+Incrediblebug20@users.noreply.github.com>
2022-03-26 03:45:58 -0500

    Add files via upload

---
+++ Products.php
@@ -0,0 +1,126 @@
+<?php
+include('includes/dbconnection.php');
+session_start();
+error_reporting(0);
+    if(isset($_POST['submit']))
+  {
+
+$name=$_POST['name'];
+    $phone=$_POST['phone'];
+    $email=$_POST['email'];
+    $message=$_POST['message'];
+ $query=mysqli_query($con,"insert into tblcontact(Name,MobileNumber,Email,Message) value('$name','$phone','$email','$message')");
+
+    if ($query) {
+    echo "<script>alert('Your message was sent successfully!.');</script>";
+echo "<script>window.location.href ='index.php'</script>";
+  }
+  else
+    {
+       echo '<script>alert("Something Went Wrong. Please try again")</script>';
+    }
+
+  
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <title>Courier Management System|| Home Page</title>
+   
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,700|Oswald:400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+    <div id="overlayer"></div>
+    <div class="loader">
+      <div class="spinner-border text-primary" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+ <?php include_once('includes/header.php');?>
+
+ 
+
+      <div class="ftco-blocks-cover-1">
+        
+        <!-- END .ftco-cover-1 -->
+        <div class="ftco-service-image-1 pb-5">
+          <div class="container">
+            <div class="owl-carousel owl-all">
+              <div class="service text-center">
+                <a href="#"><img src="images/cargo_sea_small.jpg" alt="Image" class="img-fluid"></a>
+                <div class="px-md-3">
+                  <h3><a href="#">Sea Freight</a></h3>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+                </div>
+              </div>
+              <div class="service text-center">
+                <a href="#"><img src="images/cargo_air_small.jpg" alt="Image" class="img-fluid"></a>
+                <div class="px-md-3">
+                  <h3><a href="#">Air Freight</a></h3>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+                </div>
+              </div>
+              <div class="service text-center">
+                <a href="#"><img src="images/cargo_delivery_small.jpg" alt="Image" class="img-fluid"></a>
+                <div class="px-md-3">
+                  <h3><a href="#">Package Forwarding</a></h3>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/aos.js"></script>
+
+    <script src="js/main.js"></script>
+
+
+  </body>
+
+</html>
---
+++ pricecal.php
@@ -0,0 +1,90 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+?>
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <title>Courier Management System|| Parcel Tracking</title>
+   
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,700|Oswald:400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+    <div id="overlayer"></div>
+    <div class="loader">
+      <div class="spinner-border text-primary" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+<?php include_once('includes/header.php');?>
+
+  
+
+
+
+
+      <div class="site-section bg-light" id="pricing-section">
+        <div class="container">
+         
+<div class="row text-left">
+
+
+<h4 align="center" style="color:red"> Estimated Shipping Price is: 10 USD </h4>
+    <!-- /.row -->
+
+  </div>
+
+
+        </div>
+      </div>
+
+
+   
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/aos.js"></script>
+
+    <script src="js/main.js"></script>
+
+
+  </body>
+
+</html>
